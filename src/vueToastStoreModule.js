5830a26139b4c621c3f99835ccc4436dac2f8fdd
Chon <ychongsaytc@gmail.com>
2021-11-19 13:54:07 +0800

    fix: auto close specified item

--- src/vueToastStoreModule.js
+++ src/vueToastStoreModule.js
@@ -12,7 +12,8 @@ const generateID = function () {
 
 export default {
   state: {
-    items: []
+    items: [],
+    timers: {}
   },
   getters: {
     vueToastItems (state) {
@@ -26,36 +27,42 @@ export default {
 
       // commit to open
       context.commit(MUTATION_TYPE_VUE_TOAST_OPEN, Object.assign({ _id: id }, payload))
-
-      // timer to delete the item
-      setTimeout(() => {
-        context.commit(MUTATION_TYPE_VUE_TOAST_CLOSE, { id })
-      }, (payload.ttl))
     },
 
     vueToastClose (context, payload) {
-      context.commit(MUTATION_TYPE_VUE_TOAST_CLOSE, payload)
+      // commit to close
+      context.commit(MUTATION_TYPE_VUE_TOAST_CLOSE, { _id: payload.id })
     }
   },
   mutations: {
     [MUTATION_TYPE_VUE_TOAST_OPEN] (state, payload) {
       state.items.push(payload)
+
+      // timer to delete the item
+      state.timers[payload._id] = setTimeout(() => {
+        // find specified item and delete
+        for (let i = state.items.length - 1; i >= 0; i--) {
+          if (state.items[i]._id === payload._id) {
+            state.items.splice(i, 1)
+          }
+        }
+        clearTimeout(state.timers[payload._id])
+        delete state.timers[payload._id]
+      }, payload.ttl)
     },
 
     [MUTATION_TYPE_VUE_TOAST_CLOSE] (state, payload) {
       if (state.items.length <= 0) return
 
-      if (typeof payload === 'object' && payload !== null && 'id' in payload && payload.id > 0) {
-        // specified id to close
-        const id = payload.id
-
-        // find that item
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i]._id === id) {
-            // delete item
+      if (typeof payload === 'object' && payload !== null && typeof payload._id === 'string') {
+        // find specified item and delete
+        for (let i = state.items.length - 1; i >= 0; i--) {
+          if (state.items[i]._id === payload._id) {
             state.items.splice(i, 1)
           }
         }
+        clearTimeout(state.timers[payload._id])
+        delete state.timers[payload._id]
       } else {
         // delete the last item
         state.items.pop()
